d4b46e49d3628bbadaf847d98c98073d8d6fb079
Williams C <wilcontrerasf@gmail.com>
2024-03-24 21:50:20 -0300

    Update con mui
    * se agrega el browserrouter
    * se deja el tema a ocupar

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,5 +1,10 @@
+import { AppRouter } from "./router/AppRouter"
+import { AppTheme } from "./theme"
+
 export const JournalApp = () => {
     return (
-        <h1>Hola Mundo</h1>
+        <AppTheme>
+            <AppRouter />
+        </AppTheme>
     )
 }
