feeb0dff96855f001472681caadbb1a9d0360b15
Williams C <wilcontrerasf@gmail.com>
2024-03-25 21:40:29 -0300

    Journal Views
    * agregadas las views del journal

---
+++ src/journal/views/NothingSelectedView.jsx
@@ -0,0 +1,29 @@
+import { StarOutlined } from "@mui/icons-material"
+import { Grid, Typography } from "@mui/material"
+
+export const NothingSelectedView = () => {
+    return (
+        <Grid 
+            container
+            spacing={0}
+            direction={"column"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            sx={{
+                minHeight: "calc(100vh - 110px)",
+                backgroundColor: "primary.main",
+                borderRadius: 3
+            }}
+        >
+            <Grid item xs={12}>
+                <StarOutlined sx={{fontSize: 100, color: 'white'}} />
+            </Grid>
+
+            <Grid item xs={12}>
+                <Typography variant="h5" color={"white"}>
+                    Selecciona o Crea una entreada
+                </Typography>
+            </Grid>
+        </Grid>
+    )
+}
