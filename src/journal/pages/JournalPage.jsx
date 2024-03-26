ad279499ffb9220832700e27c3da2bf0358d2a6d
Williams C <wilcontrerasf@gmail.com>
2024-03-25 21:39:54 -0300

    Journal pages-layout
    * se agrega layout del journal
    * agregado los formatos de como se veria si no hay nada y si hay algo mas el boton de agregar

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,30 @@
-import { Typography } from "@mui/material"
+import { IconButton } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from "../views"
+import { AddOutlined } from "@mui/icons-material"
 
 export const JournalPage = () => {
     return (
-        <Typography variant='h1'>JournalPage</Typography>
+        <JournalLayout>
+            <NothingSelectedView />
+            {/* <NoteView /> */}
+
+            <IconButton
+                size="large"
+                sx={{
+                    color: "white",
+                    backgroundColor: "error.main",
+                    ':hover': {
+                        backgroundColor: "error.main",
+                        opacity: 0.9
+                    },
+                    position: "fixed",
+                    bottom: 50,
+                    right: 50,
+                }}
+            >
+                <AddOutlined sx={{ fontSize: 50 }} />
+            </IconButton>
+        </JournalLayout>
     )
 }
