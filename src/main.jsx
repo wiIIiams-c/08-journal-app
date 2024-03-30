d150092e68d9cef99196b428fef11470fb1a96d3
Williams C <wilcontrerasf@gmail.com>
2024-03-29 21:51:53 -0300

    Update main
    agregada la seccion de provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { JournalApp } from './JournalApp.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from'react-redux'
+import { store } from './store'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
