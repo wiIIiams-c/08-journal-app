e66c045a084a5e1c9834086208df0a92873ce383
Williams C <wilcontrerasf@gmail.com>
2024-03-30 21:59:54 -0300

    Update AppRouter
    * se agrega el checking auth

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,16 @@
 import { Routes, Route } from'react-router-dom'
 import { AuthRoutes } from '../auth/routes/AuthRoutes'
 import { JournalRoutes } from '../journal/routes/JournalRoutes'
+import { useSelector } from 'react-redux'
+import { CheckingAuth } from '../../ui'
 
 export const AppRouter = () => {
+    const {status} = useSelector(state => state.auth)
+
+    if(status === 'checking'){
+        return <CheckingAuth />
+    }
+
     return (
         <Routes>
             <Route path='/auth/*' element={<AuthRoutes />} />
