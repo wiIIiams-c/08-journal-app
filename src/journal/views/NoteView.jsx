de08d7e31be77d456c2ba7d632cca396a62bdc7b
Williams C <wilcontrerasf@gmail.com>
2024-03-31 22:17:36 -0300

    Update journal/views
    * estilos de animacion al mostrarse

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -4,7 +4,7 @@ import { ImageGallery } from "../components"
 
 export const NoteView = () => {
     return (
-        <Grid container direction={'row'} justifyContent={'space-between'} alignItems={'center'} sx={{mb: 1}}>
+        <Grid container direction={'row'} justifyContent={'space-between'} alignItems={'center'} sx={{mb: 1}} className="animate__animated animate__fadeIn animate__faster">
             <Grid item>
                 <Typography fontSize={39} fonstweight={'ligth'}>
                     28 de agosto 2024
