78010e5c916239cf4d1c73504abde1ee90b6be1f
Williams C <wilcontrerasf@gmail.com>
2024-03-31 22:20:27 -0300

    Update thunks
    * funcion que llama al logout de firebase y luego despacha el logout

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,4 @@
-import { loginWithEmailPassword, registerUserWithEmailPassword, signInWithGoogle } from '../../firebase/providers'
+import { loginWithEmailPassword, logoutFirebase, registerUserWithEmailPassword, signInWithGoogle } from '../../firebase/providers'
 import { checkAuth, login, logout } from './'
 
 export const checkingAuth = (email, password) => {
@@ -47,5 +47,13 @@ export const startLoginWithEmailPassword = ({email, password}) => {
         if(!result.ok) return dispatch(logout(result))
 
         dispatch(login(result))
+    }
+}
+
+export const startLogout = () => {
+    return async(dispatch) => {
+        await logoutFirebase()
+
+        dispatch(logout())
     }
 }
