380e1d868cdc159e4d0df75d136ef8692068d4a0
Williams C <wilcontrerasf@gmail.com>
2024-04-01 21:15:23 -0300

    Update JournalPage
    * funcionalidad del boton de agregar se deja funcionando al guardar, por ahora solo notas estaticas
    
    * dependiente de si hay notas se muestra la pantallas correspondientes

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,14 +2,28 @@ import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
 import { AddOutlined } from "@mui/icons-material"
+import { useDispatch, useSelector } from "react-redux"
+import { startNewNote } from "../../store/journal"
 
 export const JournalPage = () => {
+    const dispatch = useDispatch()
+    const {isSaving, active} = useSelector(state => state.journal)
+
+    const onClickNewNote = () => {
+        dispatch(startNewNote())
+    }
+
     return (
         <JournalLayout>
-            <NothingSelectedView />
-            {/* <NoteView /> */}
+            {
+                (!!active)
+                ? <NoteView />
+                : <NothingSelectedView />
+            }
 
             <IconButton
+                onClick={onClickNewNote}
+                disabled={isSaving}
                 size="large"
                 sx={{
                     color: "white",
