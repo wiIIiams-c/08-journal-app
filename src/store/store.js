ef4043e0ed194ad18979aa5c6f626b139449ab1a
Williams C <wilcontrerasf@gmail.com>
2024-04-01 21:17:36 -0300

    Update store
    * agregado el journal

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { authSlice } from './auth'
+import { journalSlice } from './journal'
 
 export const store = configureStore({
     reducer: {
         // Add reducers here
         auth: authSlice.reducer,
+        journal: journalSlice.reducer,
     },
 })
