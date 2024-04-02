4279c94d5b513454f44117ff4587f49d1cde4655
Williams C <wilcontrerasf@gmail.com>
2024-04-01 21:12:49 -0300

    Update journal/components
    * se agrega al index ImageGallery y SideBarItem
    
    * en sidebar se reemplaza el listitem por un componente personalizado -SidebBarItem- y se obtiene las notas
    
    * se creo el -SideBarItem- en donde se valida que el texto si es largo (>17) lo acorte

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,9 +1,11 @@
 import { TurnedInNot } from "@mui/icons-material"
 import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
 import { useSelector } from "react-redux"
+import { SideBarItem } from "./"
 
 export const SideBar = ({drawerWidth = 240}) => {
     const {displayName} = useSelector(state => state.auth)
+    const {notes} = useSelector(state => state.journal)
 
     return (
         <Box 
@@ -26,18 +28,8 @@ export const SideBar = ({drawerWidth = 240}) => {
                 <Divider/>
 
                 <List>
-                    {['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'].map((text) => (
-                        <ListItem key={text} disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <TurnedInNot />
-                                </ListItemIcon>
-                                <Grid container>
-                                    <ListItemText primary={text} />
-                                    <ListItemText secondary={'Fugiat quis sit labore deserunt id culpa esse tempor occaecat aliquip occaecat.'} />
-                                </Grid>
-                            </ListItemButton>
-                        </ListItem>
+                    {notes.map((note) => (
+                        <SideBarItem key={note.id} {...note}/>
                     ))}
                 </List>
             </Drawer>
