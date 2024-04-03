b0b7a00e30a484cb04f4fc333e1d1b409e1e0727
Williams C <wilcontrerasf@gmail.com>
2024-04-02 22:34:31 -0300

    Update store/journal
    * update thunks con las funciones de subida de fotos y guardar la modificacion de nota
    
    * update del slice con los reducers de asignar las fotos a la nota activa, actualizar la nota, estado de guardando

--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -25,15 +25,30 @@ export const journalSlice = createSlice({
         },
         setActiveNote: (state, action) => {
             state.active = action.payload
+            state.messageSaved = ''
         },
         setNotes: (state, action) => {
             state.notes = action.payload
         },
         setSaving: (state) => {
-
+            state.isSaving = true
+            state.messageSaved = ''
         },
         updateNote: (state, action) => {
+            state.isSaving = false
+            state.notes = state.notes.map(note => {
+                if(note.id === action.payload.id){
+                    return action.payload
+                }
 
+                return note
+            })
+
+            state.messageSaved = `${action.payload.title}, actualizada correctamente`
+        },
+        setPhotosToActiveNote: (state, action) => {
+            state.active.imageUrls = [...state.active.imageUrls, ...action.payload]
+            state.isSaving = false
         },
         deleteNoteById: (state, action) => {
 
@@ -48,5 +63,6 @@ export const {
     setNotes,
     setSaving,
     updateNote,
+    setPhotosToActiveNote,
     deleteNoteById
 } = journalSlice.actions
--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,7 +1,7 @@
 import { collection, doc, setDoc } from "firebase/firestore/lite"
 import { FirebaseDB } from "../../firebase/config"
-import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes } from "./"
-import { loadNotes } from "../../helpers"
+import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes, setPhotosToActiveNote, setSaving, updateNote } from "./"
+import { fileUpload, loadNotes } from "../../helpers"
 
 export const startNewNote = () => {
     return async(dispatch, getState) => {
@@ -33,5 +33,40 @@ export const startLoadingNotes = () => {
 
         const notes = await loadNotes(uid)
         dispatch(setNotes(notes))
+    }
+}
+
+export const startSaveNote = () => {
+    return async(dispatch, getState) => {
+        dispatch(setSaving())
+        //obtengo el uid del usuario autenticado
+        const {uid} = getState().auth
+        //obtencion de la nota activa
+        const {active:note} = getState().journal
+
+        const noteToFireStore = {...note}
+        delete noteToFireStore.id
+
+        const docRef = doc(FirebaseDB, `${uid}/journal/notes/${note.id}`)
+        await setDoc(docRef, noteToFireStore, {merge: true})
+
+        dispatch(updateNote(note))
+    }
+}
+
+export const startUploadingFiles = (files) => {
+    return async(dispatch) => {
+        dispatch(setSaving())
+
+        //await fileUpload(files[0])
+        const fileUploadPromises = []
+
+        for (const file of files) {
+            fileUploadPromises.push(fileUpload(file))
+        }
+
+        const photosUrls = await Promise.all(fileUploadPromises)
+
+        dispatch(setPhotosToActiveNote(photosUrls))
     }
 }
