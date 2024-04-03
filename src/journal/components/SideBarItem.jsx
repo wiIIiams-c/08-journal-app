54d78570a0f814a1101d4682cba7f24658fc89d7
Williams C <wilcontrerasf@gmail.com>
2024-04-02 22:41:50 -0300

    Update SideBarItem
    * se agrega funcion de seleccionar nota la cual invoca el reducer de setActiveNote

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -1,15 +1,23 @@
 import { TurnedInNot } from "@mui/icons-material"
 import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
 import { useMemo } from "react"
+import { setActiveNote } from "../../store/journal"
+import { useDispatch } from "react-redux"
 
-export const SideBarItem = ({title = '', body, id}) => {
+export const SideBarItem = ({title = '', body, id, date, imageUrls = []}) => {
+    const dispatch = useDispatch()
+
+    const onClickNote = () => {
+        dispatch(setActiveNote({title, body, id, date, imageUrls}))
+    }
+
     const newTitle = useMemo(() => {
         return title.length > 17 ? title.substring(0,17) + '...' : title
     }, [title])
 
     return (
         <ListItem disablePadding>
-            <ListItemButton>
+            <ListItemButton onClick={onClickNote}>
                 <ListItemIcon>
                     <TurnedInNot />
                 </ListItemIcon>
