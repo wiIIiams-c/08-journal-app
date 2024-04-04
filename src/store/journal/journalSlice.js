2043a0a7e1f71f5e32131e0cc901be55b804b977
Williams C <wilcontrerasf@gmail.com>
2024-04-03 21:29:17 -0300

    Update journalSlice
    * reducer para limpiar las notas al hacer logout
    * reducer de borrado de nota
    * asignar las fotos a la nota activa

--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -50,19 +50,27 @@ export const journalSlice = createSlice({
             state.active.imageUrls = [...state.active.imageUrls, ...action.payload]
             state.isSaving = false
         },
+        clearNotesLogout: (state) => {
+            state.isSaving = false
+            state.messageSaved = ''
+            state.notes = []
+            state.active = null
+        },
         deleteNoteById: (state, action) => {
-
+            state.active = null
+            state.notes = state.notes.filter(note => note.id !== action.payload)
         }
     }
 })
 
 export const {
-    savingNewNote,
     addNewEmptyNote,
+    clearNotesLogout,
+    deleteNoteById,
+    savingNewNote,
     setActiveNote,
     setNotes,
+    setPhotosToActiveNote,
     setSaving,
     updateNote,
-    setPhotosToActiveNote,
-    deleteNoteById
 } = journalSlice.actions
