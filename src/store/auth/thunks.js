d0d5c9ea2b53e8721157bca9338c241a1aae4d6e
Williams C <wilcontrerasf@gmail.com>
2024-04-03 21:25:59 -0300

    Update auth/thunks
    * al hacer logout se manda a llamar el store de react para que deje limpie todo y lo deje a 0

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,5 @@
 import { loginWithEmailPassword, logoutFirebase, registerUserWithEmailPassword, signInWithGoogle } from '../../firebase/providers'
+import { clearNotesLogout } from '../journal'
 import { checkAuth, login, logout } from './'
 
 export const checkingAuth = (email, password) => {
@@ -54,6 +55,7 @@ export const startLogout = () => {
     return async(dispatch) => {
         await logoutFirebase()
 
+        dispatch(clearNotesLogout())
         dispatch(logout())
     }
 }
