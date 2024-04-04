f10c1f2b847da7e493ccc3113123bfc63c5b81f8
Williams C <wilcontrerasf@gmail.com>
2024-04-03 21:28:14 -0300

    Update journal/thunks
    * funcion que ejecuta el borrado de una nota del journal

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,6 +1,6 @@
-import { collection, doc, setDoc } from "firebase/firestore/lite"
+import { collection, deleteDoc, doc, setDoc } from "firebase/firestore/lite"
 import { FirebaseDB } from "../../firebase/config"
-import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes, setPhotosToActiveNote, setSaving, updateNote } from "./"
+import { addNewEmptyNote, deleteNoteById, savingNewNote, setActiveNote, setNotes, setPhotosToActiveNote, setSaving, updateNote } from "./"
 import { fileUpload, loadNotes } from "../../helpers"
 
 export const startNewNote = () => {
@@ -14,6 +14,7 @@ export const startNewNote = () => {
         const newNote = {
             title: '',
             body: '',
+            imageUrls: [],
             date: new Date().getTime(),
         }
 
@@ -68,5 +69,17 @@ export const startUploadingFiles = (files) => {
         const photosUrls = await Promise.all(fileUploadPromises)
 
         dispatch(setPhotosToActiveNote(photosUrls))
+    }
+}
+
+export const startDeletingNote = () => {
+    return async(dispatch, getState) => {
+        const {uid} = getState().auth
+        const {active:note} = getState().journal
+
+        const docRef = doc(FirebaseDB, `${uid}/journal/notes/${note.id}`)
+        await deleteDoc(docRef)
+
+        dispatch(deleteNoteById(note.id))
     }
 }
